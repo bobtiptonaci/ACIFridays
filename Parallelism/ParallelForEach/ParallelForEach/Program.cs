e33b3baccf9f7902723c6495b793ea4465417143
Bob Tipton <bob.tipton@aciwebs.com>
2016-07-01 08:52:56 -0400

    PLinq example added

--- Parallelism/ParallelForEach/ParallelForEach/Program.cs
+++ Parallelism/ParallelForEach/ParallelForEach/Program.cs
@@ -13,6 +13,7 @@ namespace ParallelForEach {
         testList.Add(i);
       }
 
+      Console.WriteLine("foreach example, no query, no parallelism...");
       System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
       stopwatch.Start();
       foreach (var item in testList) {
@@ -22,12 +23,40 @@ namespace ParallelForEach {
       Console.WriteLine("Elapsed time {0} ms.", stopwatch.ElapsedMilliseconds);
       Console.ReadLine();
 
+      Console.WriteLine("Parallel.ForEach example...");
       stopwatch.Reset();
       stopwatch.Start();
       Parallel.ForEach(testList, item => Process(item));
       stopwatch.Stop();
       Console.WriteLine("Elapsed time {0} ms.", stopwatch.ElapsedMilliseconds);
       Console.ReadLine();
+
+
+      Console.WriteLine("PLinq example...");
+      stopwatch.Reset();
+      stopwatch.Start();
+      var parallelQuery = from item in testList.AsParallel()
+                          where item % 10 == 0
+                          select item;
+      parallelQuery.ForAll((e) => Process(e));
+      stopwatch.Stop();
+      Console.WriteLine("Elapsed time {0} ms.", stopwatch.ElapsedMilliseconds);
+      Console.ReadLine();
+
+      Console.WriteLine("Linq example...");
+      stopwatch.Reset();
+      stopwatch.Start();
+      var linqQuery = from item in testList
+                          where item % 10 == 0
+                          select item;
+      foreach (var item in linqQuery) {
+        Process(item);
+      }
+      stopwatch.Stop();
+      Console.WriteLine("Elapsed time {0} ms.", stopwatch.ElapsedMilliseconds);
+      Console.ReadLine();
+
+
     }
 
     public static void Process(int i) {
